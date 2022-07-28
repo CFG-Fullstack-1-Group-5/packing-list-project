15b69df4f41d83e75bd5a1bf30db89c4eb4e429f
Rachel Gordon <100480990+RaiGordon@users.noreply.github.com>
2022-07-28 16:36:28 +0100

    Add files via upload

---
+++ go-packing-project/src/components/location/location.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
+import "./location.css";
+
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handleOnChange = (DData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      theme={(theme) => ({
+        ...theme,
+        borderRadius: "25px 0px 0px 25px",
+  
+      })}
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+
+export default Search;
