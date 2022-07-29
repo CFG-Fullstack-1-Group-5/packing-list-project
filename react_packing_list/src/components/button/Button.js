4c0e6cac8efb5c5f6d5894d6d1b68799411ffb0e
bodl3178 <joanna.bek@bodleian.ox.ac.uk>
2022-07-29 16:52:44 +0200

    Remove react-packing-project

--- react_packing_list/src/components/button/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ type = 'button', onClick, children }) => {
-  return (
-    <button type={type} onClick={onClick}>
-      Create my List
-    </button>
-  );
-};
-
-export default Button; 
