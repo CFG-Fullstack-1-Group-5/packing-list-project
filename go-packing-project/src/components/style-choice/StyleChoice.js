7cfa9601a2834cebceda2ecb3e236d3c2cbbcc59
Rachel Gordon <100480990+RaiGordon@users.noreply.github.com>
2022-08-01 20:44:52 +0100

    styling changes

--- go-packing-project/src/components/style-choice/StyleChoice.js
+++ go-packing-project/src/components/style-choice/StyleChoice.js
@@ -1,27 +1,24 @@
 import * as React from "react";
 import user from "../../assets/icons/user.svg";
 import "./StyleChoice.css";
-
+import Select from 'react-select'
 export default function ComboBox(props) {
+
+  const styles = [
+    { value: 'Masculine', label: 'Masculine' },
+    { value: 'Feminine', label: 'Feminine' },
+    { value: 'Both', label: 'Both' }
+  ]
+
   return (
     <>
+     <div className="style">
       <img className="icon" src={user} alt="icon" />
-      <select className="style">
-        <option defaultValue="Style">
-          Style
-        </option>
-        <option value="Masculine">Masculine</option>
-        <option value="Feminine">Feminine</option>
-        <option value="Both">Both</option>
-        theme={(theme) => ({
-      ...theme,
-      borderRadius: 0,
-      colors: {
-        ...theme.colors,
-        primary: 'black',
-      },
-    })}
-      </select>
+      <Select 
+      options={styles}
+      placeholder="Style"
+     />
+      </div>
     </>
   );
 }
