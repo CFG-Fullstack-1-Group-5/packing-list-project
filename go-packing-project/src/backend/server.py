e61e49d7e48580895456911d306c37cb2e070d6c
bodl3178 <joanna.bek@bodleian.ox.ac.uk>
2022-08-02 07:14:24 +0200

    Add backend files
    Currently based on static json, to be replace with form from frontend

---
+++ go-packing-project/src/backend/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from list_return import input_retrieved
+from db_access import necessary_clothes
+
+
+# Initializing flask app
+app = Flask(__name__)
+  
+  
+# Route for seeing a data
+@app.route('/data')
+def get_weather():
+    return jsonify(input_retrieved)
+
+@app.route('/clothes')
+def get_clothes():
+    # Returning an api for showing in  reactjs
+    return jsonify(necessary_clothes)
+    
+# Running app
+if __name__ == '__main__':
+    app.run(debug=True)
