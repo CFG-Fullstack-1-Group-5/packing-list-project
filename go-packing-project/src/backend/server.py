61d0e2d46a5d575bca0129f4786afedd7d39244e
bodl3178 <joanna.bek@bodleian.ox.ac.uk>
2022-08-02 10:32:26 +0200

    Add CORS to communicate backend with frontend

--- go-packing-project/src/backend/server.py
+++ go-packing-project/src/backend/server.py
@@ -1,18 +1,22 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 from list_return import input_retrieved
 from db_access import necessary_clothes
 
 
 # Initializing flask app
 app = Flask(__name__)
+cors = CORS(app)
   
   
 # Route for seeing a data
 @app.route('/data')
+@cross_origin()
 def get_weather():
     return jsonify(input_retrieved)
 
 @app.route('/clothes')
+@cross_origin()
 def get_clothes():
     # Returning an api for showing in  reactjs
     return jsonify(necessary_clothes)
