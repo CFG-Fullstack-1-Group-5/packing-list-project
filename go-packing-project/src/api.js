e7797a61a2d693c140f30c663dd57f0f0c06419e
bodl3178 <joanna.bek@bodleian.ox.ac.uk>
2022-08-07 12:28:24 +0200

    Add working api key

--- go-packing-project/src/api.js
+++ go-packing-project/src/api.js
@@ -10,5 +10,5 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 
 export const WEATHER_API_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services"
-export const WEATHER_API_KEY = "LMLYK8NQ9UZ4HRAU89EQ8FFQU"
+export const WEATHER_API_KEY = "EKGWNKZ42D6UFTCEDJJVNLCGD"
 export const WEATHER_API_PARAMS = "include=days&elements=datetime,temp,description,conditions,icon"
