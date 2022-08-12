9eea3e80a50d800ca47da4e45d24b551510f1dea
bodl3178 <joanna.bek@bodleian.ox.ac.uk>
2022-08-12 09:56:38 +0200

    neaten pages

--- go-packing-project/src/pages/About.js
+++ go-packing-project/src/pages/About.js
@@ -1,38 +1,41 @@
-import { Link, useNavigate } from "react-router-dom";
-import Header from "../components/header/Header";
+import Header from "../components/Header/Header";
 import React from "react";
-import AboutCard from "../components/about-card/AboutCard";
+import AboutCard from "../components/AboutCards/AboutCards";
 import "./About.css";
-
 
 function AboutPage() {
   return (
     <>
-      <Header />
+      <Header /> 
 
       <div className="Welcome">
         <h1 className="heading">About us</h1>
       </div>
       <section className="about-cards-list">
-        <AboutCard 
-            teamMemberName="Joanna"
-            teamMemberInfo="Hi, I’m Joanna, a librarian that is trying her luck in programming. The GoPacking project presented a lot of interesting challenges and it was very rewarding to work through them. And hopefully thanks to it I will for once know what clothes suitable for the weather of my destination to pack."
+        <AboutCard
+          // Joanna About Me Info
+          teamMemberName="Joanna"
+          teamMemberInfo="Hi, I’m Joanna. A librarian trying her luck in programming. The GoPacking project presented a lot of interesting challenges that were very rewarding to work through. Hopefully, thanks to it, I will for once know what clothes are suitable to pack for the weather of my destination."
         />
-                <AboutCard 
-            teamMemberName="Rachel B"
-            teamMemberInfo="Hi! I’m Rachel, a full stack student and career switcher. I have a real passion for travelling and clothes so GoPacking was an exciting project to be part of. My favourite part of the GoPacking app is the checklist - because if you're anything like me you’ll forget to pack something without it!"
+        <AboutCard
+          // Rachel B About Me Info
+          teamMemberName="Rachel B"
+          teamMemberInfo="Hi! I’m Rachel, a full stack student and career switcher. I have a real passion for travelling and clothes, so GoPacking was an exciting project to be a part of. My favourite part of the app is the checklist - because if you're anything like me, you’ll forget to pack something without it!"
         />
-                <AboutCard 
-            teamMemberName="Laura"
-            teamMemberInfo="Hello, I’m Laura, a junior full stack web developer with a  background in the arts and interiors. As a compulsive list-maker, the GoPacking project has really appealed to my love of organisation and I’ve enjoyed keeping it as streamlined and intuitive as possible. "
+        <AboutCard
+          // Laura About Me Info
+          teamMemberName="Laura"
+          teamMemberInfo="Hello, I’m Laura, a junior full stack web developer with a  background in the arts and interiors. As a compulsive list-maker, the GoPacking project has really appealed to my love of organisation and I’ve enjoyed keeping it as streamlined and intuitive as possible. "
         />
-                <AboutCard 
-            teamMemberName="Rachel G"
-            teamMemberInfo="I’m Rachel, a junior full-stack software engineer who transitioned from a career within the financial industry. Working on this project and helping to create a tool that I know I would have greatly benefited from in the past has been an enjoyable way to bring my love of both travelling and learning about new technologies together. "
+        <AboutCard
+          // Rachel G About Me Info
+          teamMemberName="Rachel G"
+          teamMemberInfo="I’m Rachel, an aspiring full-stack developer, transitioning from a career within the financial industry. Working on this project has been a rewarding and enjoyable challenge, that has allowed me to bring my love of both travelling and technology together."
         />
-                <AboutCard 
-            teamMemberName="Kavita"
-            teamMemberInfo="Hi, I’m Kavita! I’m switching careers from research to software development.  I love to travel to new places but packing to cover all weather scenarios can be a tricky task, so GoPacking has been the perfect project to be a part of! I love the simplicity of our design."
+        <AboutCard
+          // Kavita About Me Info
+          teamMemberName="Kavita"
+          teamMemberInfo="Hi, I’m Kavita! I’m switching careers from research to software development.  I love to travel to new places but packing to cover all weather scenarios can be a tricky task, so GoPacking has been the perfect project to be a part of! I love the simplicity of our design."
         />
       </section>
     </>
