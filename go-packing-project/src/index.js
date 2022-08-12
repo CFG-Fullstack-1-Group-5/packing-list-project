6bc3141809b3214b99be24e8bbdb937b9bd61930
bodl3178 <joanna.bek@bodleian.ox.ac.uk>
2022-08-12 10:13:43 +0200

    Neaten index

--- go-packing-project/src/index.js
+++ go-packing-project/src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Fashion from "./pages/Fashion";
 import Packing from "./pages/Packing";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -18,9 +15,8 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
-        <Route path="/fashion" element={<Fashion />} />
         <Route path="/packing" element={<Packing />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode> 
 );
