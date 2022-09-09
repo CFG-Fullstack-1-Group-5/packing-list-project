0112c441c5b053c255e4f1497cb2fc9b9efdfe66
Joanna Bek <66479882+asikowe@users.noreply.github.com>
2022-09-09 09:32:21 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '46c50a5d3dmsh9ab94cb6c333a2fp1c0669jsn5ce077c6107d',
+		'X-RapidAPI-Key': 'your-API-key',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -10,5 +10,5 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 // API details for WEATHER
 export const WEATHER_API_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services"
-export const WEATHER_API_KEY = "EKGWNKZ42D6UFTCEDJJVNLCGD"
+export const WEATHER_API_KEY = "your-API-key"
 export const WEATHER_API_PARAMS = "include=days,current&elements=datetime,temp,feelslike,windspeed,humidity,description,conditions,icon" 
